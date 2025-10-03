7a7d0111df949589cfb396f79da86c4a20d89c0a
Ariq Bagus Sugiharto <ariqbagus19@gmail.com>
2025-10-03 18:52:12 +0700

    Initial Commit

---
+++ src/app/hello.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Contoh fungsi penjumlahan sederhana"""
+    return a + b
+
+
+if __name__ == "__main__":
+    print("Hello VS Code!")
+    print("2 + 3 =", add(2, 3))
---
+++ tests/test_add.py
@@ -0,0 +1,5 @@
+from app.hello import add  # pyright: ignore[reportMissingImports]
+
+
+def test_add():
+    assert add(2, 3) == 5
